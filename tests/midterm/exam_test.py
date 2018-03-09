c5d0f4bdd830d8555f852a38c5fc133d28b58758
Miguelh1997 <35585352+Miguelh1997@users.noreply.github.com>
2018-03-08 21:55:43 -0600

    Update exam_test.py

--- tests/midterm/exam_test.py
+++ tests/midterm/exam_test.py
@@ -1,5 +1,5 @@
 import unittest
-from exam import get_miles_per_hour, get_bonus_pay_amount, reverse_string, get_list_min_max
+from src.midterm.exam import get_miles_per_hour, get_bonus_pay_amount, reverse_string, get_list_min_max
 #write import statements for exam functions
 
 class Test_Midterm(unittest.TestCase):
