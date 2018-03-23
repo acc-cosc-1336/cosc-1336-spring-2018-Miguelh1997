a246ba6131fa69e679c63197ac62b40bec64a930
Miguelh1997 <35585352+Miguelh1997@users.noreply.github.com>
2018-03-22 22:23:49 -0500

    Add files via upload

---
+++ tests/homework/test_homework7.py
@@ -0,0 +1,40 @@
+import unittest
+from src.homework.homework7 import pdistance, pdistance_matrix
+
+#write import statement for homework 7 file
+
+
+class TestHomework7(unittest.TestCase):
+
+    def test_sample(self):
+        self.assertEqual(1,1)
+
+    def test_pdistance(self):
+        self.assertEqual(['0.00000', '0.40000', '0.10000', '0.10000', 
+                           '0.40000','0.00000', '0.40000', '0.30000',
+                           '0.10000', '0.40000', '0.00000', '0.20000',
+                           '0.10000', '0.30000', '0.20000', '0.00000'], pdistance_matrix([
+     ['T','T','T','C','C','A','T','T','T','A'],
+     ['G','A','T','T','C','A','T','T','T','C'],
+     ['T','T','T','C','C','A','T','T','T','T'],
+     ['G','T','T','C','C','A','T','T','T','A']
+    ]))
+
+    #create a test for get p distance matrix with following data
+    '''
+    Sample Dataset
+    [
+     ['T','T','T','C','C','A','T','T','T','A'],
+     ['G','A','T','T','C','A','T','T','T','C'],
+     ['T','T','T','C','C','A','T','T','T','T'],
+     ['G','T','T','C','C','A','T','T','T','A']
+    ]
+    
+    Sample Output
+    0.00000 0.40000 0.10000 0.10000
+    0.40000 0.00000 0.40000 0.30000
+    0.10000 0.40000 0.00000 0.20000
+    0.10000 0.30000 0.20000 0.00000
+    '''
+unittest.main(verbosity=2)
+    
