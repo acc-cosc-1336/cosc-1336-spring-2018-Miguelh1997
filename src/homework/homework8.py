077768f2bc46066a423ca0d79435ef720186431a
Miguelh1997 <35585352+Miguelh1997@users.noreply.github.com>
2018-03-28 19:57:25 -0500

    Add files via upload

---
+++ src/homework/homework8.py
@@ -0,0 +1,40 @@
+'''
+Write a function named add_inventory with a widgets parameter of type dictionary, widget_name, and quantity
+
+The function will add the widget to the dictionary if it doesn't exist.
+If the widget exists it will update the quantity of the widgets.
+
+    :param widgets:    a dictionary of widgets. Key is widget name, value is quantity
+    :param widget_name: Widget name
+    :param quantity:    Running count of inventory on hand
+    :return:
+'''
+
+def add_inventory(dictionary, widget_name, quantity):
+    dictionary.update({widget_name:quantity})
+    if widget_name in dictionary:
+        value = dictionary[widget_name]
+
+    return value
+    
+    
+    
+
+
+'''
+Write a function named remove_inventory_widget with a widget_name parameter.
+
+The function will remove the widget_name if it exists and return 'Record deleted'
+Otherwise it returns 'Item not found'
+
+    :param widgets:
+    :param widget_name:
+    :return:
+'''
+def remove_inventory_widget(dictionary, widget_name): 
+    if widget_name in dictionary:
+        del dictionary[widget_name]
+        return 'Record deleted'
+    else:
+        'Item not found'
+    
