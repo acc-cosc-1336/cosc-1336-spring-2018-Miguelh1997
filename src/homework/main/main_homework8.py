cbe592068048f838f2ac0efd721fcfb24b36b46f
Miguelh1997 <35585352+Miguelh1997@users.noreply.github.com>
2018-03-28 19:58:06 -0500

    Add files via upload

---
+++ src/homework/main/main_homework8.py
@@ -0,0 +1,30 @@
+'''
+Write a main function to create an empty dictionary and
+a user-controlled loop to prompt for a widget name and quantity.
+Add the values to the dictionary as key(widget name) and value(quantity) pairs.
+
+After user decides to exit write data to file .
+
+'''
+
+
+def main():
+    dictionary = {}
+    file = open('Dictionary.txt', 'w')
+    n = 'Y'
+    while n == 'Y':
+        widget = input('Please input a widget name: ')
+        quantity = int(input('What is the quantity of ' + widget + ': '))
+        if widget in dictionary:
+            dictionary[widget] += quantity
+        else:
+            dictionary[widget] = quantity
+            
+
+
+        n = input("Press 'y' to enter another widget.. ")
+        n = n.upper()
+    file.write(str(dictionary))
+    file.close()
+
+main()
