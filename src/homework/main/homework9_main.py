1156b2ba526e73788963a11066be001623c7f783
Miguelh1997 <35585352+Miguelh1997@users.noreply.github.com>
2018-04-04 21:12:06 -0500

    Add files via upload

---
+++ src/homework/main/homework9_main.py
@@ -0,0 +1,25 @@
+#write import statements for Player and Die class
+#Create an instance of the Main class and call/execute the roll_doubles method
+from die import Die
+from player import Player
+
+
+class Main:
+
+    def run(self):
+        Player().roll_doubles()
+
+
+Main().run()
+
+
+
+
+
+
+
+
+
+
+
+
