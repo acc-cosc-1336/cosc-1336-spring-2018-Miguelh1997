dc200a1af5f4830b6e197fc18a8b287af8002a41
Miguelh1997 <35585352+Miguelh1997@users.noreply.github.com>
2018-04-05 21:33:46 -0500

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -1,4 +1,4 @@
-import src.assignments.main_assignment7
+import src.assignments.assignment10.main
 
 #running this file will run the code in main_assignment7.py file without the need to modify
 #code in that file
