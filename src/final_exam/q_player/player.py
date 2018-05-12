c7c4223b62552297810fa3242196a4c2a5f257fa
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2018-05-11 21:49:55 -0500

    Blackboard submission

---
+++ src/final_exam/q_player/player.py
@@ -0,0 +1,20 @@
+class Player:
+    def __init__(self,d1,d2,p=True):
+        self.d1 = d1
+        self.d2 = d2
+    
+    def check_come_out_roll(self):
+        dsum = self.d1 + self.d2
+        if self.d1 > 6 or self.d2 > 6:
+            return 'Invalid Range'
+        elif dsum == 7 or dsum == 11:
+            return 'Winner'
+        elif dsum == 7 or dsum == 11 and p==False:
+            return'Loser'
+        elif dsum == 2 or dsum == 3 or dsum == 12 and p==True:
+            print('Loser')
+        elif dsum == 2 or dsum == 3 or dsum == 12 and p==False:
+            print('Winner')
+        else:
+            return dsum
+        
